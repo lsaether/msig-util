c9f59d47eadee0c60481b43864656d769995edaf
Logan Saether <X@logansaether.com>
2020-07-10 07:51:07 -0700

    v1.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import program from "commander";
 
-import Logger from "./Logger";
+import { deriveAddress } from "./actions/deriveAddress";
 
-const helloWorld = () => {
-  Logger.info("Hello world!");
-};
-
-program.command("start").action(helloWorld);
+program
+  .command("derive")
+  .option("--addresses <addr1,addr2,...>", "The addresses that compose the multisig (comma-separated).", "")
+  .option("--threshold <num>", "The threshold of approvals needed.", "0")
+  .option("--ss58Prefix <num>", "The prefix for the network to use for encoding.", "0")
+  .action(deriveAddress);
 
 program.parse(process.argv);
